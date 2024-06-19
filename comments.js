c4b6defe699fc9dcf34a016bcdb5b8ef2124f490
sergiodcastelan <sergioc@thegeeks.world>
2024-06-19 23:51:43 +0000

    Copilot third comment

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, () => {
+    console.log('Server started at http://localhost:3000');
+});
